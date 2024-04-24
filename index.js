77145beaddd3778b9472efaadb2b405130870c5e
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-04-24 20:52:52 +0500

    Updated port

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(bodyParser.json());
 app.use("/admin", adminRouter)
 
-const PORT = 3000;
+const PORT = 4000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
