d28cdd6ce877807d26436756853ca8fb1871ddc8
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-04-24 23:00:37 +0500

    Updated cors

--- index.js
+++ index.js
@@ -5,13 +5,8 @@ const bodyParser = require('body-parser');
 const app = express();
 const adminRouter = require("./routes")
 
-app.use(cors({
-    origin: 'https://restaurant-website-fvcyyo599-fatimairshad123s-projects.vercel.app/', // Allow only requests from this origin
-   methods: ['GET', 'POST'], // Allow only specific HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'],
-   // credentials: true // Allow only specific headers
-  }));
-//app.use(cors())
+
+app.use(cors())
 // Middleware for parsing request bodies
 app.use(bodyParser.json());
 app.use("/admin", adminRouter)
