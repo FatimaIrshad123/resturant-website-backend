1803301aebd3a9993e5d541a1a497930e0cd390e
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-04-24 22:29:43 +0500

    Updated cors

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const adminRouter = require("./routes")
 app.use(cors({
     origin: 'http://localhost:5173/', // Allow only requests from this origin
     methods: ['GET', 'POST'], // Allow only specific HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allow only specific headers
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true // Allow only specific headers
   }));
 //app.use(cors())
 // Middleware for parsing request bodies
