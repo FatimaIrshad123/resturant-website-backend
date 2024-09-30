25fae44a54eeabf136248f7101865137e7676120
Fatima Irshad <154042550+FatimaIrshad123@users.noreply.github.com>
2024-09-30 20:28:49 +0500

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://Fatima:Fatima@cluster0.7bunznc.mongodb.net/Resturant-website');
+mongoose.connect('/Resturant-website');
 
 const AdminSchema = mongoose.Schema({
     username : String,
